b5d724b037b62c4938525def1939a9e9b4b8d072
rauanpm <faizullayevrauan@mail.ru>
2019-02-14 09:04:50 +0600

    firstcom

---
+++ Lab 1/Task 1/Task 1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Programm
+    {
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());                                      // Get the number of numbers
+
+            int cnt = 0;                                                                   // Create counter of prime numbers
+
+            int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse); // Create an array of read numbers
+
+            List<int> arrPrime = new List<int>();                                             // Create a List for prime numbers
+
+            for (int i = 0; i < arr.Length; i++)
+            {                                             // Check each element of the array 
+
+                if (IsPrime(arr[i]))
+                {                                                         // Call the method IsPrime, checking whether a number is prime
+
+                    cnt++;                                                                 // Increment prime counter
+
+                    arrPrime.Add(arr[i]);                                                     // Add prime number to the prime numbers' array
+
+                }
+
+            }
+
+            Console.WriteLine(cnt);                                                        // Output the total number of prime numbers
+
+            foreach (int el in arrPrime)
+            {                                                     // Loop through the array
+
+                Console.Write(el + " ");
+
+            }
+
+        }
+
+        static bool IsPrime(int a)
+        {                                                          // Create a function to check if a number is prime
+
+            if (a == 1)
+            {
+
+                return false;
+
+            }
+
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+            {
+
+                if (a % i == 0)
+                {
+
+                    return false;
+
+                }
+
+            }
+
+            return true;
+
+        }
+
+    }
+
+}
+
---
+++ Lab 1/Task 2/Task 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Task2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Student s1 = new Student("Rauan Faizullayev", "18bd11030");
+            s1.NewYear();
+
+        }
+
+    }
+
+    class Student
+    {
+
+        string Name { get; set; }
+
+        string Id { get; set; }
+
+        int Year { get; set; }
+
+        public Student(string name, string id)
+        {
+
+            Name = name;
+
+            Id = id;
+
+            Year = 2019;
+
+        }
+
+        public void NewYear()
+        {
+
+            Console.WriteLine("Student: {0}; Id: {1}", Name, Id);
+
+            Year++;
+
+        }
+        Console.ReadKey();
+
+}
