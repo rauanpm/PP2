799b54f841addd1be0da38a4a67dd99e0ec02be6
rauanpm <faizullayevrauan@mail.ru>
2019-02-16 02:06:28 +0600

    PP2

--- Lab 1/Task 1/Task 1/Program.cs
+++ Lab 1/Task 1/Task 1/Program.cs
@@ -4,74 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task1
+namespace ConsoleApp2
 {
-    class Programm
+    class Program
     {
-
         static void Main(string[] args)
         {
-
-            int n = Convert.ToInt32(Console.ReadLine());                                      // Get the number of numbers
-
-            int cnt = 0;                                                                   // Create counter of prime numbers
-
-            int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse); // Create an array of read numbers
-
-            List<int> arrPrime = new List<int>();                                             // Create a List for prime numbers
-
-            for (int i = 0; i < arr.Length; i++)
-            {                                             // Check each element of the array 
-
-                if (IsPrime(arr[i]))
-                {                                                         // Call the method IsPrime, checking whether a number is prime
-
-                    cnt++;                                                                 // Increment prime counter
-
-                    arrPrime.Add(arr[i]);                                                     // Add prime number to the prime numbers' array
-
+            int q = Convert.ToInt32(Console.ReadLine());                                         // Get the number of numbers
+            int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse);   // Create an array of read numbers
+            int cnt = 0;                                                                       // Create counter to prime numbers
+            List<int> p = new List<int>();                                                    // Create list for prime numbers
+            for (int i = 0; i < arr.Length; ++i)                                             // Check each element of the array
+            {
+                if (isPrime(arr[i]))                                                        // Call the method IsPrime, checking whether a number is prime
+                {
+                    cnt++;
+                    p.Add(arr[i]);
                 }
-
+            }
+            Console.WriteLine(cnt);
+            for (int i = 0; i < p.Count; ++i)
+            {
+                Console.Write(p[i] + " ");
             }
 
-            Console.WriteLine(cnt);                                                        // Output the total number of prime numbers
+            Console.ReadKey();
 
-            foreach (int el in arrPrime)
-            {                                                     // Loop through the array
-
-                Console.Write(el + " ");
-
-            }
 
         }
-
-        static bool IsPrime(int a)
-        {                                                          // Create a function to check if a number is prime
-
+        static bool isPrime(int a)
+        {                                                                         // Create a function to check if a number is prime
             if (a == 1)
             {
-
                 return false;
-
             }
-
             for (int i = 2; i <= Math.Sqrt(a); i++)
             {
-
                 if (a % i == 0)
                 {
-
                     return false;
-
                 }
-
             }
-
             return true;
-
         }
-
     }
-
 }
-
